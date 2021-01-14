e0f7849a1037916280196dbd2ded835580e73c40
Maxime Chagnon <luffy-star-93@hotmail.fr>
2021-01-14 15:17:42 +0100

    update

--- script.js
+++ script.js
@@ -1,21 +1,53 @@
 
 
-// var songsToAdd = [
-//     {
-//       "name": "Dior",
-//       "artist": "Pop Smoke",
-//       "url": "./assets/music/pop_smoke_dior.mp3",
-//       "cover_art_url": "./assets/pop_smoke_dior_cover.jpg"
-//     },
-//   ];
+var songsToAdd = [
+    {
+      "name": "Intro",
+      "artist": "Lil Wayne",
+      "url": "./assets/music/pop_smoke_dior.mp3",
+      "cover_art_url": "./assets/pop_smoke_dior_cover.jpg"
+    },
+    {
+      "name": "Blunt Blowin",
+      "artist": "Lil Wayne",
+      "url": "./assets/music/pop_smoke_dior.mp3",
+      "cover_art_url": "./assets/pop_smoke_dior_cover.jpg"
+    },
+    {
+      "name": "MegaMan",
+      "artist": "Lil Wayne,",
+      "url": "./assets/music/pop_smoke_dior.mp3",
+      "cover_art_url": "./assets/pop_smoke_dior_cover.jpg"
+    },
+    {
+      "name": "6 Foot 7 Foot",
+      "artist": "Lil Wayne, Cory Gunz",
+      "url": "./assets/music/pop_smoke_dior.mp3",
+      "cover_art_url": "./assets/pop_smoke_dior_cover.jpg"
+    },
+    {
+      "name": "Nightmares Of The Bottom",
+      "artist": "Lil Wayne",
+      "url": "./assets/music/pop_smoke_dior.mp3",
+      "cover_art_url": "./assets/pop_smoke_dior_cover.jpg"
+    },
+    {
+      "name": "She Whill",
+      "artist": "Lil Wayne, Drake",
+      "url": "./assets/music/pop_smoke_dior.mp3",
+      "cover_art_url": "./assets/pop_smoke_dior_cover.jpg"
+    },
+  ];
+
+var playlist = document.querySelector('playlist');
 
 
-// METHODE PERSO AUTO MUSIC
 var play = document.querySelector('#play');
 var song1 = document.querySelector('#song1')
 var like_icon = document.querySelector('#like_album')
 
 
+// Fonction like/dislike
 like_icon.addEventListener("click", () => {
     // Update the Button
     if( like_icon.classList.contains('far')){
@@ -26,8 +58,10 @@ like_icon.addEventListener("click", () => {
         like_icon.classList.remove('fas');
         like_icon.classList.add('far');
     }
+    playlist.style.background = 'green';
 });
 
+// Fonction play/pause
 play.addEventListener("click", () => {
     
     // Update the Button
@@ -37,7 +71,8 @@ play.addEventListener("click", () => {
     // Update the Audio
     var method = pause ? 'pause' : 'play';
     yourAudio[method]();
-
+    
+    // add class musique en cours de lecture
     if (song1.className === 'played_song'){
         song1.classList.remove("played_song");
     }
@@ -48,3 +83,8 @@ play.addEventListener("click", () => {
     // Prevent Default Action
     return false;
 });
+
+
+// Fonctions add to tableau
+
+    
